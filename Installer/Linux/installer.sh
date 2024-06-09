cc434677fcf9b7a0605c7fb7731a86599040caa2
Fedi6431 <102946457+Fedi6431@users.noreply.github.com>
2024-06-09 09:44:10 +0200

    Add files via upload

---
+++ Installer/Linux/installer.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+url="https://download.oracle.com/java/22/latest/jdk-22_linux-x64_bin.deb"
+output="/home/username/Desktop/jdk-22_linux-x64_bin.deb"
+
+wget "$url" -O "$output"
+
+cd /home/username/Desktop/
+./jdk-22_linux-x64_bin.deb
