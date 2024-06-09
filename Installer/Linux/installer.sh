596ca6fc3d86a063e2b4c8a8be6fa3405297abe9
Fedi6431 <102946457+Fedi6431@users.noreply.github.com>
2024-06-09 10:08:52 +0200

    Update installer.sh

--- Installer/Linux/installer.sh
+++ Installer/Linux/installer.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+username=$USER
 url="https://download.oracle.com/java/22/latest/jdk-22_linux-x64_bin.deb"
-output="/home/username/Desktop/jdk-22_linux-x64_bin.deb"
+output="/home/$username/Desktop/jdk-22_linux-x64_bin.deb"
 
 wget "$url" -O "$output"
 
-cd /home/username/Desktop/
+cd /home/$username/Desktop/
 
 chmod +x jdk-22_linux-x64_bin.deb
 sudo ./jdk-22_linux-x64_bin.deb
