ca35b3c6e493d577327a1a4dd8b67dd8b18dc0bc
Fedi6431 <102946457+Fedi6431@users.noreply.github.com>
2024-06-09 09:34:04 +0200

    Update installer.cmd

--- installer.cmd
+++ installer.cmd
@@ -1,5 +1,5 @@
 @echo off
 set url=https://download.oracle.com/java/22/latest/jdk-22_windows-x64_bin.exe
-set output=C:\Users\%username\Desktop
+set output=C:\Users\%username%\Desktop\jdk-22_windows-x64_bin.exe
 
 powershell.exe -Command "& {$url = '%url%'; $output = '%output%'; Invoke-WebRequest -Uri $url -OutFile $output}"
