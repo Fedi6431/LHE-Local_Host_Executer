30d2868a28b81f544b900842d08a91f827d77145
Fedi6431 <102946457+Fedi6431@users.noreply.github.com>
2024-06-13 16:10:07 +0200

    Update installer.sh

--- Installer/Linux/installer.sh
+++ Installer/Linux/installer.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
+banner="What are you using:
+1 Linux
+2 Tremux
+"
 
-username=$USER
+read -p -n 1 "Enter system choice:  " system
 
-sudo apt update
-sudo apt upgrade
-#sudo apt full-upgrade
-sudo apt autoremove
+if [ $system = 1 ]; then
+    username=$USER
+    
+    sudo apt update -y 
+    sudo apt full-upgrade -y
+    sudo apt autoremove -y
+    
+    cd /home/$username/Desktop/
+    sudo apt install default-jdk
+    java -version
+    
+    echo Now you can use LHE-Local_Host_Executer made by Fedi6431
 
-cd /home/$username/Desktop/
-sudo apt install default-jdk
-java -version
+elif [ $system = 2 ]; then
+    apt update -y
+    apt full-upgrade -y 
+    apt autoremove -y
 
-echo Now you can use LHE-Local_Host_Executer made by Fedi6431
+    pkg install openjdk -y
+    java -version
+    echo Now you can use LHE-Local_Host_Executer made by Fedi6431
