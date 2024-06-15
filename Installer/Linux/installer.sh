43810e9406010ff30c386a9d2b1c228c60a178e0
Fedi6431 <102946457+Fedi6431@users.noreply.github.com>
2024-06-15 23:05:13 +0200

    Update installer.sh

--- Installer/Linux/installer.sh
+++ Installer/Linux/installer.sh
@@ -1,27 +1,21 @@
 #!/bin/bash
-
 banner="What are you using:
 1) Linux 
 2) Termux
 "
-
 echo "$banner"
-
 read -p "--> " usr_ch
-
 if [[ $usr_ch = 1 ]]; then
   username=$USER
   # Update the system and remove old packages
   sudo apt update -y
   sudo apt full-upgrade -y 
   sudo apt autoremove -y
-  
   # Install OpenJDK and git
   sudo apt install openjdk -y
   sudo apt install git
   # Check the Java version of OpenJDK
   java -version
-  
   # change the directory to the main dir
   cd /home/$username/desktop
   # Code that will go in the installer.sh
@@ -30,7 +24,6 @@ if [[ $usr_ch = 1 ]]; then
   do
     echo -e "Select option\n1) Install\n2) Update\n3) Remove\n4) Close"
     read -p "--> " user_choice
-    
     if [ "\$user_choice" = 1 ]; then
       git clone https://github.com/Fedi6431/LHE-Local_Host_Executer.git
       echo -e "\\nLHE installed successfully!\\n"
@@ -57,20 +50,17 @@ EOF
   # Make the installer.sh executable and run it
   chmod +x installer.sh
   ./installer.sh
-
 elif [[ $usr_ch = 2 ]]; then
   # Update the system and remove old packages
   pkg update -y
   apt update -y
   apt full-upgrade -y
   apt autoremove -y
-  
   # Install OpenJDK and git
   pkg install openjdk
   pkg install git
   # Check the Java version of OpenJDK
   java -version
-  
   # change the directory to the main dir
   cd ~
   # Code that will go in the installer.sh
@@ -79,7 +69,6 @@ elif [[ $usr_ch = 2 ]]; then
   do
     echo -e "Select option\n1) Install\n2) Update\n3) Remove\n4) Close"
     read -p "--> " user_choice
-    
     if [ "\$user_choice" = 1 ]; then
       git clone https://github.com/Fedi6431/LHE-Local_Host_Executer.git
       echo -e "\\nLHE installed successfully!\\n"
@@ -100,12 +89,10 @@ elif [[ $usr_ch = 2 ]]; then
     read -p "Press Enter to continue"
     clear
   done
-  
 EOF
   # clear apt outputs
   clear
   # Make the installer.sh executable and run it
   chmod +x installer.sh
   ./installer.sh
-
 fi
