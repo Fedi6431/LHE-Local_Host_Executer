a970752e0d7276d9b8e6b06913f8c0214ce74943
Fedi6431 <102946457+Fedi6431@users.noreply.github.com>
2024-06-26 18:59:32 +0200

    Update Client.java

--- src/Client.java
+++ src/Client.java
@@ -78,6 +78,6 @@ public class Client {
     }
 
     public static void main(String args[]) {
-        Client client = new Client("192.168.1.185", 5000);
+        Client client = new Client("127.0.0.1", 5000);
     }
 }
