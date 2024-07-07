d9bc6a02205aa815e50b979aaa2d21ce17ed8a99
Fedi6431 <102946457+Fedi6431@users.noreply.github.com>
2024-07-07 15:00:50 +0200

    Update Server.java

--- src/Server.java
+++ src/Server.java
@@ -7,69 +7,62 @@ public class Server {
     public static void Initialize(String address, int port) {
         try {
             // create a server socket
-
             InetAddress localIpAddress = InetAddress.getLocalHost();
-            System.out.println("Made by Fedi6431");
             ServerSocket server = new ServerSocket();
             System.out.println("Server started");
+            
             server.bind(new InetSocketAddress(localIpAddress, 65000));
             System.out.println("Running on port " + port + ", address: " + localIpAddress);
-
-
 
             // establish a connection with the client
             Socket socket = server.accept();
             System.out.println("Client connected");
 
-
             // get input from the terminal
             DataInputStream input = new DataInputStream(
                     new BufferedInputStream(socket.getInputStream()));
 
-
             // string to read input
             String line = "";
 
-            line = input.readUTF();
-            boolean linux = Boolean.parseBoolean(line);
+            String os = System.getProperty("os.name");
+            if (os.equals("windows")) {
+            while (!line.equalsIgnoreCase("exit")) {
+                try {
+                    // read the message sent by the client via socket
+                    line = input.readUTF();
+                    System.out.println(line);
 
-            if (linux == true) {
-                while (!line.equalsIgnoreCase("exit")) {
-                    try {
-                        // read the message sent by the client via socket
-                        line = input.readUTF();
-                        System.out.println(line);
-
-                        // try to execute the command on terminal using Runtime.getRuntime
-                        Process linux_process = Runtime.getRuntime().exec(new String[]{line});
-                    } catch (IOException IOe) {
-                        System.out.println("Oops.. Something went wrong.");
-                        System.out.println("IO exception: " + IOe.getMessage());
-                        System.out.println("Exception cause" + IOe.getCause());
+                    // try to execute the command on terminal using Runtime.getRuntime
+                    Process linux_process = Runtime.getRuntime().exec(new String[]{line});
+                } catch (IOException IOe) {
+                    System.out.println("Oops.. Something went wrong.");
+                    System.out.println("IO exception: " + IOe.getMessage());
+                    System.out.println("Exception cause" + IOe.getCause());
                     }
                 }
-            } else if (linux == false) {
-                while (!line.equalsIgnoreCase("exit")) {
-                    try {
-                        // read the message sent by the client via socket
-                        line = input.readUTF();
-                        System.out.println(line);
 
-                        // try to execute the command on cmd using Runtime.getRuntime
-                        Process windows_process = Runtime.getRuntime().exec(new String[]{"cmd", "/c " + line});
-                    } catch (IOException IOe) {
-                        System.out.println("Oops.. Something went wrong.");
-                        System.out.println("IO exception: " + IOe.getMessage());
-                        System.out.println("Exception cause" + IOe.getCause());
-                    }
+         } else { 
+            while (!line.equalsIgnoreCase("exit")) {
+                try {
+                    // read the message sent by the client via socket
+                    line = input.readUTF();                        
+                    System.out.println(line);
+
+                    // try to execute the command on cmd using Runtime.getRuntime
+                    Process windows_process = Runtime.getRuntime().exec(new String[]{"cmd", "/c " + line});
+                } catch (IOException IOe) {
+                    System.out.println("Oops.. Something went wrong.");
+                    System.out.println("IO exception: " + IOe.getMessage());
+                    System.out.println("Exception cause" + IOe.getCause());
                 }
             }
-
-
-            // close the connection
-            System.out.println("Made by Fedi6431");
-            socket.close();
-            input.close();
+        }
+        // close the connection
+        System.out.println("Made by Fedi6431");
+        socket.close();
+        input.close();
+            
         }
         // handle any errors
         catch (IOException IOe) {
@@ -84,6 +77,7 @@ public class Server {
 
     public static void main(String args[]) {
         try {
+            System.out.println("Made by Fedi6431");
             InetAddress localIpAddress = InetAddress.getLocalHost();
             String address = localIpAddress.getHostAddress();
             Initialize(address,65000);
