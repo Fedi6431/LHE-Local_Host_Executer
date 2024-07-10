8eb1bdc73b80ae47f9832855d469a953c94c4e93
Fedi6431 <102946457+Fedi6431@users.noreply.github.com>
2024-07-10 13:01:42 +0200

    Update Client.java

--- src/Client.java
+++ src/Client.java
@@ -1,24 +1,35 @@
 //This software is protected by Fedi6431© copyrights
 
+//This software is protected by Fedi6431© copyrights
+
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
 import java.io.*;
 import java.net.*;
-import java.util.Scanner;
 
 public class Client {
     public static void Initialize(String address, int port) {
         try {
+            System.out.println("Made by Fedi6431\n");
+
             // establish a connection with the server
             Socket socket = new Socket(address, port);
 
             // get input from the terminal
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            BufferedReader ServerInput = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedReader ServerInfo = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
             // send output to the server socket
             DataOutputStream output = new DataOutputStream(socket.getOutputStream());
 
             // string to read input
             String line = "";
+            String os = ServerInput.readLine();
+            String info = ServerInfo.readLine();
 
+            System.out.println(info);
+
+            System.out.println("\nServer system: " + os + "\n");
             // IP of the server and the client
             String serverIP = socket.getLocalAddress().getHostAddress();
             String clientIP = Inet4Address.getLocalHost().getHostAddress();
@@ -28,11 +39,11 @@ public class Client {
                 // get user input
                 System.out.print(clientIP + " to " + serverIP + "$-");
                 line = reader.readLine();
-                
+
                 // send the received input to the server
                 output.writeUTF(line);
             }
-            
+
             // close the connection
             output.close();
             socket.close();
@@ -48,13 +59,15 @@ public class Client {
         }
     }
 
-    public static void main(String args[]) {
 
-        System.out.println("Made by Fedi6431");
-        System.out.println("Type server IPv4");
-        Scanner scanner = new Scanner(System.in);
-        String address = scanner.nextLine();
-
-        Initialize(address, 65000);
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("LHE-Local_Host_Executer 1.7\nUsage: java Client.java [Options] {target specification}\nCONNECTION:\n  -IPv4 <server ipv4> Connect to the target server\nEXAMPLES:\n  java Client.java -IPv4 192.168.255.255\n");
+        } else if (args[0].equals("-IPv4")) {
+            String address = args[1];
+            int port = 65000;
+            Initialize(address, port);
+        }
     }
 }
+
